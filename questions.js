d006ce51aecb65196f9c8b81a3b6516d62cf8851
CASSAR SUZY <contact@scassar.com>
2023-10-04 10:36:50 +0200

    modif script.js ajout score et questions.js

---
+++ questions.js
@@ -0,0 +1,64 @@
+
+
+const gameboy = document.querySelector(".game-boy");
+
+const questionsList = [
+    {
+    numQuestion: "Question 1",
+    question: "Avant d'être rappeur, Tupac a démarrer sa carrière comme?",
+    answer1: {reponse: "Danseur comédien", id: "reponse1_question1"},
+    answer2: "Roady danseur",
+    answer3: "Comédien Roady", 
+    lien: "https://fr.wikipedia.org/wiki/Tupac_Shakur",
+    },
+   
+    {
+    numQuestion: "Question 2",
+    question: "Quel était le premier nom du jeu Pac-Man ?",
+    answer1: "Poc-Man",
+    answer2: "Pack-Man",
+    answer3: "Puc-Man", 
+    lien: "https://fr.wikipedia.org/wiki/Pac-Man",
+    },
+   
+    {
+    numQuestion: "Question 3",
+    question: "Quel groupe français interprête des musiques de jeu ?",
+    answer1: "Pixxel", 
+    answer2: "Méga Ran",
+    answer3: "The Mégas",
+    lien: "https://fr.wikipedia.org/wiki/Pac-Man",
+    },
+    
+    {
+    numQuestion: "Question 4",
+    question: "Quel est le premier jeu Open World ?",
+    answer1: "Elite", 
+    answer2: "Pokémon",
+    answer3: "Zelda",
+    lien: "https://fr.wikipedia.org/wiki/Elite_(jeu_vidéo)",
+    },
+    
+    {
+    numQuestion: "Question 5",
+    question: "Qui a été le premier rappeur blanc ?",
+    answer1: "Beasties Boy", 
+    answer2: "Falco", 
+    answer3: "Eminem", 
+    lien: "https://fr.wikipedia.org/wiki/Falco_(chanteur)",
+    },
+    
+    {
+    numQuestion: "Question 6",
+    question: "Koji Kondo est le premier compositeur de musique de jeu. De quelle musique de jeu est-il à l'origine ?",
+    answer1: "The Legend of Zelda", 
+    answer2: "Final Fantasy", 
+    answer3: "Super Mario Bros", 
+    lien: "https://fr.wikipedia.org/wiki/Tupac_Shakur",
+    },
+]
+
+
+
+
+export default questionsList;
---
+++ score.js
@@ -0,0 +1,30 @@
+
+import arrayPoints from "./script.js";
+
+//Calcul et affichage des points
+
+//j'ai un tableau avec les points de chaque bonne réponse
+// 1. je veux addditionner les points de mon tableau
+// 2. je veux afficher la somme de ces points dans la div score
+
+
+
+
+// const scoreTitle = document.querySelector(".score");
+// const result = document.createElement("p");
+// result.classList.add("score");
+// result.innerText = "prout";
+// scoreTitle.appendChild(result);
+    
+
+
+//arrayPoints.reduce((acc, currentValue) => acc + currentValue);
+
+
+//const sum = 0;
+// for (let i = 0; i < arrayPoints.length; i++) {
+//      sum += arrayPoints[i];
+    
+// }
+
+
