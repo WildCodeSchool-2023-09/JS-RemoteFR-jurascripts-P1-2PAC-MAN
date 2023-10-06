9c5bec2d86c9f074145242b606b20506a1309616
Skarial <145289682+Skarial@users.noreply.github.com>
2023-10-06 09:57:45 +0200

    Update script.js
    / asset en moins

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const rightAnswer_question4 = document.querySelector("#reponse1_question4")
 const rightAnswer_question5 = document.querySelector("#reponse2_question5")
 const rightAnswer_question6 = document.querySelector("#reponse3_question6")
 const badAnswer = document.querySelectorAll(".bad_answer")
-const badAudio = new Audio("assets/pacman_death.mp3")
+const badAudio = new Audio("pacman_death.mp3")
 const goodAnswer = document.querySelectorAll(".good_answer")
-const goodAudio = new Audio("assets/pacman_beginning.mp3")
-const finalAudio = new Audio("assets/2Pac_me_against_the_world.mp3")
+const goodAudio = new Audio("pacman_beginning.mp3")
+const finalAudio = new Audio("2Pac_me_against_the_world.mp3")
 
 goodAudio.volume = 0.02
 badAudio.volume = 0.02
