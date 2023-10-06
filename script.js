f7d177b6431176f62bbdd83c3e9749bcb50dbd08
Skarial <145289682+Skarial@users.noreply.github.com>
2023-10-06 09:50:52 +0200

    Update script.js
    ./

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log("coucou");
 const rightAnswer = document.querySelector(".good_answer")
 const rightAnswer_question1 = document.querySelector("#reponse2_question1")
 const rightAnswer_question2 = document.querySelector("#reponse3_question2")
@@ -7,10 +6,10 @@ const rightAnswer_question4 = document.querySelector("#reponse1_question4")
 const rightAnswer_question5 = document.querySelector("#reponse2_question5")
 const rightAnswer_question6 = document.querySelector("#reponse3_question6")
 const badAnswer = document.querySelectorAll(".bad_answer")
-const badAudio = new Audio("/assets/pacman_death.mp3")
+const badAudio = new Audio("./assets/pacman_death.mp3")
 const goodAnswer = document.querySelectorAll(".good_answer")
-const goodAudio = new Audio("/assets/pacman_beginning.mp3")
-const finalAudio = new Audio("/assets/2Pac_me_against_the_world.mp3")
+const goodAudio = new Audio("./assets/pacman_beginning.mp3")
+const finalAudio = new Audio("./assets/2Pac_me_against_the_world.mp3")
 
 goodAudio.volume = 0.02
 badAudio.volume = 0.02
